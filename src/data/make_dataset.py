f9d1cff7922b7c8c977fcf12412d4af0b7a07a49
Julio Oliveira <jcalvesoliveira@hotmail.com>
2022-09-21 18:42:14 -0400

    adding dependencies and processing annotation data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,19 +1,41 @@
 # -*- coding: utf-8 -*-
+import os
 import click
 import logging
+import pandas as pd
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+
+
+def read_multiple_csvs(path: Path) -> pd.DataFrame:
+    """Read multiple csv files into a single dataframe
+
+    Args:
+        path (Path): Path to the directory containing the csv files
+
+    Returns:
+        pandas.DataFrame: Dataframe containing all the csv files
+    """
+    dfs = []
+    for file in os.listdir(path):
+        if file.endswith(".csv"):
+            new_df = pd.read_csv(path / file)
+            new_df['filename'] = file[0:-4]
+            dfs.append(new_df)
+    return pd.concat(dfs)
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+@click.argument('input_folder', type=click.Path(exists=True))
+@click.argument('output_folder', type=click.Path())
+def main(input_folder, output_folder):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+    annotations_dfs = read_multiple_csvs(Path(input_folder))
+    annotations_dfs.to_csv(f'{output_folder}/annotations.csv', index=False)
 
 
 if __name__ == '__main__':
@@ -22,9 +44,5 @@ if __name__ == '__main__':
 
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
 
     main()
